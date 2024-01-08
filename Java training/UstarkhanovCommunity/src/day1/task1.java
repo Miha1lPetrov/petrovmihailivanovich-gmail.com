eb49143342a74a7ba34eaf235de6cc4e684c7a79
Mihail Petrov <petrovmihailivanovich@gmail.com>
2024-01-08 22:46:22 +0400

    Добавил курс

---
+++ Java training/UstarkhanovCommunity/src/day1/task1.java
@@ -0,0 +1,43 @@
+package day1;
+
+public class task1 {
+    public static void main(String[] args) {
+
+        //Задача1
+//        int i = 1;
+//        while (i <=10){
+//            System.out.print("JAVA ");
+//            i++;
+//        }
+        //Задача 2 С циклом For
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.print("JAVA ");
+//        }
+
+        //Задача 3
+//        int i = 1;
+//        while (i <= 10){
+//            System.out.println("JAVA");
+//            i++;
+//        }
+
+        //Задача 4
+//        int year = 1980;
+//        while (year <= 2020){
+//            System.out.println("Олимпиада " + year + " года");
+//            year+=4;
+//        }
+        //Задача 5 с FOR
+//        for (int i = 1980; i <= 2020; i+=4) {
+//            System.out.println("Олимпиада " + i + " года");
+//        }
+
+        //Задача 6 умножение
+        int i = 1;
+        int k = 3;
+        while ( i <=10){
+            System.out.println(i + " * " + k + " = " + (k*i));
+            i++;
+        }
+    }
+}
